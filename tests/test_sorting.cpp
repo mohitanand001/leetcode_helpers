b76f156ddf51aa9d7dd9d669454288cfcb278aaa
Mohit Anand <anand.mohit001@gmail.com>
2022-12-28 02:56:51 +0530

    Create test_sorting.cpp

---
+++ tests/test_sorting.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include <sorting/sorting_using_comparators.cpp>
+using namespace std;
+
+vector<int> sortor(vector<int> &v);
+
+int main()
+{
+  vector<int> v = {5,1,2,53,112,231,1-12};
+  vector<int> tmp = v;
+  
+  tmp = sortor(tmp);
+  assert(v == tmp);
+  return 0;
+}
+
