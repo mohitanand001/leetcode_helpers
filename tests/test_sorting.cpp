388075c43dce28dc85958fd298bed071e03a4260
Mohit Anand <anand.mohit001@gmail.com>
2022-12-28 22:44:29 +0530

    Update test_sorting.cpp

--- tests/test_sorting.cpp
+++ tests/test_sorting.cpp
@@ -1,16 +1,20 @@
 #include<bits/stdc++.h>
-#include "../sorting/sorting_for_testing.cpp"
 using namespace std;
 
-// vector<int> sortor(vector<int> &v);
+int sm(int a, int b)
+{
+	cout << "a is" << a << " and b is " << b << endl;
+	return a + b;
+}
+
+
+
 
 int main()
 {
-  vector<int> v = {5,1,2,53,112,231,1-12};
-  vector<int> tmp = v;
-  
-  tmp = sortor(tmp);
-  assert(v == tmp);
-  return 0;
-}
+	int c = sm(1,5);
+	assert(6 == c); 
+	
+	return 0;
 
+}
